c862e026011073c2f880a0c8c8a251e8094d4b71
Matheus Lopes Marendino <91556477+lopesmatheus1@users.noreply.github.com>
2025-01-27 21:00:12 -0300

    feat: add Header component and banner

---
+++ app/_components/header.tsx
@@ -0,0 +1,28 @@
+import { Card, CardContent, CardFooter } from "./ui/card";
+import Image from "next/image";
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="flex items-center justify-between px-5 py-8">
+        <Link href={"/"}>
+          <Image
+            src={"/Logo.png"}
+            height={18}
+            width={120}
+            alt={"Imagem da logo da FSW barber"}
+          />
+        </Link>
+
+        <Button variant={"ghost"} size={"icon"}>
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
+import Header from "./_components/header";
+import { Input } from "@/app/_components/ui/input";
+import { Button } from "./_components/ui/button";
+import { Search } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Header />
+
+      <div className="space-y-4 p-5">
+        <div>
+          <h2 className="text-xl">
+            Olá, <span className="font-bold">Matheus</span>
+          </h2>
+          <p>Terça, 27 de janeiro</p>
+        </div>
+
+        <div className="flex items-center justify-center gap-2">
+          <Input placeholder="Buscar Barbearias" />
+          <Button className="h-10 w-10">
+            <Search size={18} />
+          </Button>
+        </div>
+
+        <div className="relative h-[150px] w-full">
+          <Image
+            src={"/Banner1.png"}
+            fill
+            className="rounded-xl object-cover"
+            alt={"Agende nos melhores com FSW barber"}
+          />
+        </div>
+      </div>
     </div>
   );
 }
