4606ed190377a3001840dbf0dcb13624bfae17c5
Lopesmatheus1 <lopesmatheus1@hotmail.com>
2025-02-03 09:00:39 -0300

    fix: removing never used errors

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -5,7 +5,7 @@ import { Input } from "./ui/input";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { redirect } from "next/navigation";
+
 import { useRouter } from "next/navigation";
 import {
   Form,
